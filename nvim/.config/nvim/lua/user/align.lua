ad84287602a4f7ab0266c52b9d6f7b7153b071c1
Martin Korff <mko@captureone.com>
2023-02-14 15:33:45 +0100

    neovim: Adding initial neovim configuration
    Using lazy plugin manager, but not really loading anything lazy

---
+++ nvim/.config/nvim/lua/user/align.lua
@@ -0,0 +1,10 @@
+local status_ok, easy_align = pcall(require, "EasyAlign")
+if not status_ok then
+	return
+end
+
+easy_align.setup({
+    -- Add custom thing here
+
+    -- Setup keyboard shortcut
+})
---
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -0,0 +1,83 @@
+local fn = vim.fn
+
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Use a protected call so we don't error out on first use
+local status_ok, lazy = pcall(require, "lazy")
+if not status_ok then
+	return
+end
+
+-- -- Autocommand that reloads neovim whenever you save the plugins.lua file
+-- vim.cmd([[
+--   augroup lazy_user_config
+--   autocmd!
+--   autocmd BufWritePost plugins.lua source <afile> | Lazy sync
+--   augroup end
+-- ]])
+
+-- Install your plugins here
+return lazy.setup({
+	"wbthomason/packer.nvim", -- Have packer manage itself
+	"nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
+	"windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+	"numToStr/Comment.nvim",
+	-- "JoosepAlviste/nvim-ts-context-commentstring",
+	"kyazdani42/nvim-web-devicons",
+	"kyazdani42/nvim-tree.lua",
+	"akinsho/bufferline.nvim",
+	"moll/vim-bbye",
+	"nvim-lualine/lualine.nvim",
+	"akinsho/toggleterm.nvim",
+	"ahmedkhalf/project.nvim",
+	"lewis6991/impatient.nvim",
+	"lukas-reineke/indent-blankline.nvim",
+	"folke/which-key.nvim",
+
+	-- Colorschemes
+    "sainnhe/gruvbox-material",
+
+	-- Show Colors
+    "nvchad/nvim-colorizer.lua",
+
+    "junegunn/vim-easy-align",
+
+	-- Completions
+	"hrsh7th/nvim-cmp",     -- The completion plugin
+	"hrsh7th/cmp-buffer",   -- buffer completions
+	"hrsh7th/cmp-path",     -- path completions
+    "hrsh7th/cmp-cmdline",  -- nvim command line completions
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-nvim-lua",
+
+	-- LSP
+	"neovim/nvim-lspconfig", -- enable LSP
+	"williamboman/mason.nvim", -- simple to use language server installer
+	"williamboman/mason-lspconfig.nvim",
+	"jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
+	"RRethy/vim-illuminate",
+
+	-- Telescope
+	"nvim-telescope/telescope.nvim",
+
+	-- Treesitter
+	"nvim-treesitter/nvim-treesitter",
+
+	-- Git
+    "tpope/vim-fugitive",
+	"lewis6991/gitsigns.nvim",
+
+    -- Tools
+    -- "Civitasv/cmake-tools.nvim",
+})
