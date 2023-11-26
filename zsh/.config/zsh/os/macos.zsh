c1fd946eff27b6905155ad85516a3493abb49c8e
Martin Korff <martin.korff@gmail.com>
2023-11-26 02:10:23 +0100

    zsh: fixing fzf bindings paths

--- zsh/.config/zsh/os/macos.zsh
+++ zsh/.config/zsh/os/macos.zsh
@@ -1,9 +1,6 @@
 # FZF
-fzf_version=""
-if type "fzf" > /dev/null; then
-    fzf_version=`fzf --version | awk '{ print $1 }'`
-
-    source /opt/homebrew/Cellar/fzf/$fzf_version/shell/key-bindings.zsh
-    source /opt/homebrew/Cellar/fzf/$fzf_version/shell/completion.zsh
+if [ -n "${commands[fzf-share]}" ]; then
+  source "$(fzf-share)/key-bindings.zsh"
+  source "$(fzf-share)/completion.zsh"
 fi
 
