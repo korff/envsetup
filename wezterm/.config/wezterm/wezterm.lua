f08120d79e0f77ac5e11f48f1ab6446f45d9b1b2
Martin Korff <martin.korff@gmail.com>
2024-11-13 15:17:40 +0100

    nvim and wezterm colorscheme update
    Using the dark version of gruvbox

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,8 +7,8 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox dark, medium (base16)'
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+-- config.color_scheme = 'Gruvbox Dark (Gogh)'
 
 -- Font
 config.font = wezterm.font('CaskaydiaMono Nerd Font', { stretch = 'Normal', weight = 'Regular' })
